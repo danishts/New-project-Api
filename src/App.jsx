473b618787ea7aca16d7a0651d15ee2e3a98695e
danishts <danishsaddique519@gmail.com>
2025-04-10 16:24:05 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Products from "./Products";
 function App() {
   return (
     <div>
-      <h1 className="text-center text-6xl text-blue-200">My Product Store</h1>
+      <h1 className="text-center text-6xl text-blue-200">ShopZone</h1>
       <Products />
     </div>
   );
